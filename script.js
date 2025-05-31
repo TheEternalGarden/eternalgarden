5821078c825243ce0b2f288f2cf30745b4f9890b
ETERNAL LABS <eternal@gardenx.xyz>
2025-05-30 20:19:26 -0700

    Initial commit: Basic website with hamburger menu

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerIcon = document.querySelector('.hamburger-icon');
+    
+    hamburgerIcon.addEventListener('click', () => {
+        // We'll add menu functionality later
+        console.log('Hamburger menu clicked');
+    });
+}); 
